ab13dcdcf5e4eb54b0cf3d91cb9a4e3b0025edec
K570U\Katherine506 <katherine.gonzalez.arias@gmail.com>
2021-08-23 23:32:46 -0600

    last  commit on monday 23

--- ui.frontend/src/components/_shared/Button/Button.jsx
+++ ui.frontend/src/components/_shared/Button/Button.jsx
@@ -5,9 +5,22 @@ import styles from './Button.module.scss';
 import cx from 'classnames';
 import { linkService } from '@konrad/reweb-aem';
 import { BUTTON_STYLES, LINK_TYPES, ICONS_PATH } from '../../../lib/constants';
+const block = 'btn';
 
 const Button = (props) => {
-    return <span>Button Placeholder</span>;
+    const {url, text } = props;
+
+    const block = 'btn';
+
+    return(
+        <div className={`${block}`}>
+            <div className={`${block}__container`}>
+            <a className={`${block}__container-ref`} href={url}>
+                <button className={`${block}__container-btn`}>Kat</button>
+            </a>
+            </div>
+        </div>
+    )
 };
 
 Button.displayName = 'Button';
